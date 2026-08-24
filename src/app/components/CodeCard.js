85c32da94f9df7ceb3a241d461cc83b29e078418
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-08-24 08:39:32 +0200

    Restrcture code black

--- src/app/components/CodeCard.js
+++ src/app/components/CodeCard.js
@@ -3,17 +3,19 @@ import PlayButton from "./PlayButton";
 export default function CodeCard({children, extras, colour, codeColour}) {
     return (
         <div className={` ${extras}
-        w-[75%] min-h-[10rem] max-h-[30rem] rounded-3xl 
+        w-[75%] min-h-[12rem] max-h-[40rem] space-y-5 p-10 
         flex items-center shrink-0 flex-col overflow-hidden
-        shadow-2xl`} >
+        `} >
 
-            <div className={`w-full flex-1 min-h-0 ${codeColour}
-            flex p-12 overflow-scroll text-black text-xl font-mono`} >   
+            <div className={`w-full flex-1 min-h-0 ${codeColour} rounded-2xl
+            flex p-12 overflow-scroll text-black text-xl font-mono
+            inset-shadow-sm inset-shadow-neutral-500`} >   
                 {children}
             </div>
 
-            <div className={`mt-auto ${colour} h-[4rem] w-full flex items-center p-5
-             flex-row-reverse gap-3`}>
+            <div className={` ${colour} rounded-full h-[4rem] w-[50%] flex 
+            justify-center items-center p-5
+              shadow-xl`}>
                 <PlayButton/>
             </div>
 
